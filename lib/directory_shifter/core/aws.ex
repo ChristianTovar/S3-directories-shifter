96f7c72ee294b38b6a7c4821be66048a075d9bb5
ChristianTovar <tovarchristian21@gmail.com>
2020-03-16 14:53:07 -0500

    added initial objects documentation

--- lib/directory_shifter/core/aws.ex
+++ lib/directory_shifter/core/aws.ex
@@ -3,9 +3,13 @@ defmodule DirectoryShifter.Core.Aws do
   Contains AWS S3 support for working with buckets.
   """
 
+  @doc """
+  Returns a list with the first 1000 objects from the bucket.
+  """
+  @spec get_initial_objects(String.t()) :: [String.t()]
   def get_initial_objects(bucket) do
     bucket
-    |> ExAws.S3.list_objects()
+    |> ExAws.S3.list_objects(max_keys: 1000)
     |> ExAws.request()
     |> extract_only_route()
   end
