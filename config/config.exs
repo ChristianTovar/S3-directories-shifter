c1939121df0331c85bb770ba576d713692bb952c
ChristianTovar <tovarchristian21@gmail.com>
2020-03-16 16:27:25 -0500

    added object in-memory fetching from origin bucket

--- config/config.exs
+++ config/config.exs
@@ -2,15 +2,19 @@ import Config
 
 config :cloudinex,
   debug: false,
-  base_url: "base-url",
-  api_key: "api-key",
-  secret: "secret",
-  cloud_name: "cloud-name"
+  base_url: "BASE_URL",
+  api_key: "API_KEY",
+  secret: "SECRET",
+  cloud_name: "CLOUD_NAME"
 
 config :ex_aws,
   debug_requests: true,
   access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
   secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
   region: {:system, "AWS_REGION"}
+
+config :directory_shifter,
+  origin_bucket: "ORIGIN_BUCKET",
+  destiny_bucket: "DESTINY_BUCKET"
 
 import_config "#{Mix.env()}.exs"
--- lib/directory_shifter/core/aws.ex
+++ lib/directory_shifter/core/aws.ex
@@ -6,15 +6,32 @@ defmodule DirectoryShifter.Core.Aws do
   @doc """
   Returns a list with the first 1000 objects from the bucket.
   """
-  @spec get_initial_objects(String.t()) :: [String.t()]
-  def get_initial_objects(bucket) do
-    bucket
+  @spec get_initial_objects :: [String.t()]
+  def get_initial_objects do
+    origin_bucket()
     |> ExAws.S3.list_objects(max_keys: 1000)
     |> ExAws.request()
     |> extract_only_route()
   end
 
+  @doc """
+  Returns an in-memory image from the origin bucket by the specified route.
+  """
+  @spec get_object(String.t()) :: binary()
+  def get_object(route) do
+    origin_bucket()
+    |> ExAws.S3.get_object(route)
+    |> ExAws.request()
+    |> extract_only_object()
+  end
+
   defp extract_only_route({:ok, %{body: %{contents: contents}}}) do
     Enum.map(contents, fn %{key: route} -> route end)
   end
+
+  defp extract_only_object({:ok, %{body: image}}), do: image
+  defp extract_only_object({:error, _}), do: nil
+
+  defp origin_bucket, do: Application.fetch_env!(:directory_shifter, :origin_bucket)
+  defp destiny_bucket, do: Application.fetch_env!(:directory_shifter, :destiny_bucket)
 end
