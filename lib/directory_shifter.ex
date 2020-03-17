099e08daa724a0fb7e36ce75fcd097e929883a07
ChristianTovar <tovarchristian21@gmail.com>
2020-03-17 14:59:11 -0500

    added execution point for public API

--- lib/directory_shifter.ex
+++ lib/directory_shifter.ex
@@ -2,17 +2,32 @@ defmodule DirectoryShifter do
   @moduledoc """
   Documentation for `DirectoryShifter`.
   """
+  require Logger
+  alias DirectoryShifter.Core.Aws
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> DirectoryShifter.hello()
-      :world
-
+  Executes the main image migrations and removal until
+  the assets are completely gone on the origin bucket.
   """
-  def hello do
-    :world
+  @spec start :: any()
+  def start(acc \\ 1000) do
+    Logger.debug("Processed #{acc} files...")
+
+    case Aws.get_initial_object_routes() do
+      [] ->
+        Logger.debug("Process completed!")
+
+      routes ->
+        routes
+        |> Task.async_stream(&move_and_delete/1)
+        |> Stream.run()
+
+        start(acc + 1000)
+    end
+  end
+
+  defp move_and_delete(route) do
+    Aws.move_image(route)
+    Aws.delete_object(route)
   end
 end
--- test/directory_shifter_test.exs
+++ test/directory_shifter_test.exs
@@ -2,7 +2,7 @@ defmodule DirectoryShifterTest do
   use ExUnit.Case
   doctest DirectoryShifter
 
-  test "greets the world" do
-    assert DirectoryShifter.hello() == :world
+  test ":]" do
+    assert 1 == 1
   end
 end
